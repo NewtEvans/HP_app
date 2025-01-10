f84600835a337715a3a7f9d4df8019afb1534420
Newt <tfisera1997@gmail.com>
2025-01-10 13:04:09 +0100

    Založen komponenta pro stahování dat z API

---
+++ src/utils/api.js
@@ -0,0 +1,26 @@
+const API_URL = "https://hp-api.onrender.com/api/";
+
+const fetchData = (url, requestOption) => {
+    const apiUrl = `${API_URL}${url}`;
+
+    return fetchData(apiUrl, requestOption)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`Network response was not ok: ${response.status} ${response.statusText}`);
+            }
+
+            if (requestOption.method !== 'DELETE')
+                return response.json();
+        })
+        .catch((error) => {
+            throw error;
+        });
+};
+
+ export async function apiGet(url) {
+    const apiUrl = `${API_URL}${url}`;
+
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data;
+}
