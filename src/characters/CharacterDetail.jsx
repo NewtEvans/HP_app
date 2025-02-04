8c0764c060a9e8053663472ebc2532f787270443
Newt <tfisera1997@gmail.com>
2025-02-04 16:29:06 +0100

    Úprava detailu postavy

--- src/characters/CharacterDetail.jsx
+++ src/characters/CharacterDetail.jsx
@@ -14,7 +14,44 @@ const CharacterDetail = () => {
   return (
     <div>
       {characters.map((character) => (
-        <CharacterCard key={character.id} character={character} />
+        <div key={character.id}>
+          <h1>
+            {character.name} - {character.house}
+          </h1>
+          <hr />
+          <h3>Known as:</h3>
+          <ul>
+            {character.alternate_names.map((alternate_name) => (
+              <li key={alternate_name}>{alternate_name}</li>
+            ))}
+          </ul>
+          <br />
+          <p>
+            <strong>Species</strong>
+            <br />
+            {character.species}
+          </p>
+          <p>
+            <strong>Gender</strong>
+            <br />
+            {character.gender}
+          </p>
+          <p>
+            <strong>Birthday</strong>
+            <br />
+            {character.dateOfBirth}
+          </p>
+          <p>
+            <strong>Ancestry</strong>
+            <br />
+            {character.ancestry}
+          </p>
+          <p>
+            <strong>Eyes</strong>
+            <br />
+            {character.eyeColour}
+          </p>
+        </div>
       ))}
     </div>
   );
